42678dc1f47bd14c55c181dd8e5e7d6184f005e3
bigby5480-sketch <bigby5480@gmail.com>
2026-01-07 10:34:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,1 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("app-cache-v1").then(cache =>
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/login.html"
-      ])
-    )
-  );
-});
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
-});
